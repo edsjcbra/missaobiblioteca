65067751f5c39829d98d3379a2302f80910e43db
Ederson Silva <edsjcbra@gmail.com>
2023-01-28 07:39:23 +0000

    Added BusinessRole Class

---
+++ MissaoBiblioteca/BusinessRole.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace MissaoBiblioteca
+{
+    public abstract class BusinessRole
+    {
+        public BusinessRole(string? name, string? role_ID)
+        {
+            Name = name;
+            Role_ID = role_ID;
+        }
+
+        public string? Name { get; private set; }
+        public string? Role_ID { get; private set; }
+
+        public virtual void RegisterRole()
+        {
+
+        }
+
+        public virtual void ListRoles() { }
+    }
+}
