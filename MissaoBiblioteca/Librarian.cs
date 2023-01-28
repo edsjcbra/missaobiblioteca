93db781459b4931f7349dc97711a7718e67c8829
Ederson Silva <edsjcbra@gmail.com>
2023-01-28 07:44:47 +0000

    Added Librarian Class

---
+++ MissaoBiblioteca/Librarian.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace MissaoBiblioteca
+{
+    public class Librarian : BusinessRole
+    {
+        public Librarian(string name, string librarianId) : base(name, librarianId) { }
+
+        List<string> list = new List<string>();
+        public override void RegisterRole()
+        {
+            list.Add(this.Name);
+            list.Add(this.Role_ID);
+        }
+        public override void ListRoles()
+        {
+            foreach (var librarian in list)
+            {
+                Console.WriteLine(librarian);
+            }
+        }
+    }
+}
