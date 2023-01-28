24a4c466df333b614f56397bb5f14755a92be03f
Ederson Silva <edsjcbra@gmail.com>
2023-01-28 07:42:00 +0000

    Added Student Class

---
+++ MissaoBiblioteca/Student.cs
@@ -0,0 +1,21 @@
+﻿namespace MissaoBiblioteca
+{
+    public class Student : BusinessRole
+    {
+        public Student(string name, string studentCpf) : base(name, studentCpf) { }
+
+        List<string> list = new List<string>();
+        public override void RegisterRole()
+        {
+            list.Add(this.Name);
+            list.Add(this.Role_ID);
+        }
+        public override void ListRoles()
+        {
+            foreach (var student in list)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
