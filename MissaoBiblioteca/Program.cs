5f1a073a103f5bdb97c7dc4e3133c8d4bb838103
Ederson Silva <edsjcbra@gmail.com>
2023-01-28 07:23:48 +0000

    Versao Inicial

---
+++ MissaoBiblioteca/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MissaoBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
